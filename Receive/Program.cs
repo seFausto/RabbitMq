30ded81667c9974dea6d110dc1c97216d21816b7
Fausto Ruvalcaba <faustoruvalcaba@Faustos-MacBook-Pro.local>
2018-09-19 17:34:39 -0500

    Initial Commit

---
+++ Receive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var binFormatter = new BinaryFormatter();
+                        var mStream = new MemoryStream(ea.Body);
+
+                        var result = (List<int>)binFormatter.Deserialize(mStream);
+
+                        Console.WriteLine(result.Count);
+                    };
+
+
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using RabbitMQ.Client.Framing;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var properties = new BasicProperties();
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+
+                    var message = new List<int>
+                        {
+                            1,2,3,4,5
+                        };
+
+                    var binFormatter = new BinaryFormatter();
+                    var mStream = new MemoryStream();
+                    binFormatter.Serialize(mStream, message);
+
+                    channel.BasicPublish(string.Empty, "hello", null, mStream.ToArray());
+
+
+
+                    Console.WriteLine(" Press Enter to continue");
+
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
